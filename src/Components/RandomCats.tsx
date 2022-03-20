b9a12ee24ed5274cf8a0b2705738ee19d64eee61
Lesha! <8900800700a@gmail.com>
2022-03-20 14:56:50 +0300

    adding analytics and image autoloading
    the image is automatically loaded when the page is opened + add google analytics for btns

--- src/Components/RandomCats.tsx
+++ src/Components/RandomCats.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import Button from "./Button";
 import CatBtn from '../Images/smiling-cat-face-with-heart-shaped-eyes-emoji-by-google.png';
@@ -6,33 +6,50 @@ import Loader from  '../Images/Loader.gif'
 import {firebaseAnalytics} from "../utils/firebaseConfig";
 
 
-function RandomCats()
+const RandomCats = () =>
 {
-    const [imgUrl, setUrl] = useState('');
-    const catImg = async () => {
+    const [catImgUrl, setCatImgUrl] = useState('');
+    const GetCatImgUrlFromApi = async () => {
         firebaseAnalytics.logEvent("viewing_a_cat");
-        setUrl(Loader);
+        setCatImgUrl(Loader);
         await fetch("https://api.thecatapi.com/v1/images/search")
             .then(res => res.json())
-            .then(data => setUrl(data[0].url))
+            .then(data => setCatImgUrl(data[0].url))
     }
+        useEffect(() => {
+            GetCatImgUrlFromApi();
+        }, []);
+
+    const LikeACat = () =>
+    {
+        firebaseAnalytics.logEvent("like_cat_button");
+        GetCatImgUrlFromApi();
+    }
+
+    const NextCat = () =>
+    {
+        firebaseAnalytics.logEvent("next_cat_button");
+        GetCatImgUrlFromApi();
+    }
+
     return(
         <CatGeneratorContainer>
             <h1>Random cats!🐈</h1>
             <CatsGenerator>
                 <ButtonRow>
-                    <Button onClick={() => catImg()} color={'#F6F6F6'}>🎀 Next! 🎀</Button>
-                    <Button onClick={() => catImg()} color={'#F2E362'}><Icon src={CatBtn} alt={'Meow!'} height={'20px'} width={'20px'}/></Button> //
+                    <Button onClick={() => NextCat()} color={'#F6F6F6'}>🎀 Next! 🎀</Button>
+                    <Button onClick={() => LikeACat()} color={'#F2E362'}><Icon src={CatBtn} alt={'Meow!'} height={'20px'} width={'20px'}/></Button>
                 </ButtonRow>
                 <ImgContainer>
-                    <CatImg src={imgUrl}/>
+                    <CatImg src={catImgUrl}/>
                 </ImgContainer>
             </CatsGenerator>
         </CatGeneratorContainer>
     )
 }
 
-const CatGeneratorContainer = styled.div`{
+export default RandomCats;
+const CatGeneratorContainer = styled.div`
   margin-top: 30px;
   display: flex;
   flex-direction: column;
@@ -40,32 +57,30 @@ const CatGeneratorContainer = styled.div`{
   color: white;
   letter-spacing: 1px;
   flex-shrink: 0;
-  
-}`;
+`;
 
 const Icon = styled.img`
   display: flex;
   margin: 4px;
-`
+`;
 
-const  CatImg = styled.img`{
+const  CatImg = styled.img`
   image-rendering: high-quality;
   max-width: 100%;
   max-height: 100%;
-}`;
+`;
 
 const ImgContainer = styled.div`
   height: 55vh;
   object-fit: scale-down;
-  
-`
+`;
 
 const ButtonRow = styled.div`
   display: flex;
   width: 172px;
   justify-content: space-between;
   margin-bottom: 24px;
-`
+`;
 
 const CatsGenerator = styled.div`
   margin-top: 24px;
@@ -82,7 +97,4 @@ const CatsGenerator = styled.div`
     button{
       margin-top: 40px;
     }
-  }
-`
-
-export default RandomCats;
+  }`
